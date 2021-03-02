cf8cd89163ab9d20f29bd00fdc4266e8fcf3ef55
thewhiteninja <40250307+thewhiteninja@users.noreply.github.com>
2021-03-02 23:08:35 +0100

    add command to display S.M.A.R.T attributes

---
+++ Sources/Commands/command_smart.cpp
@@ -0,0 +1,180 @@
+
+#include "Utils/buffer.h"
+#include "Drive/disk.h"
+#include "Utils/table.h"
+#include "Utils/utils.h"
+#include "Utils/constant_names.h"
+#include "options.h"
+
+#include <algorithm>
+#include <cstdint>
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <memory>
+#include <stdexcept> 
+
+
+bool isSmartEnabled(HANDLE hDevice, DWORD ucDriveIndex)
+{
+	SENDCMDINPARAMS stCIP = { 0 };
+	SENDCMDOUTPARAMS stCOP = { 0 };
+	DWORD dwRet = 0;
+	BOOL bRet = FALSE;
+
+	stCIP.cBufferSize = 0;
+	stCIP.bDriveNumber = ucDriveIndex & 0xff;
+	stCIP.irDriveRegs.bFeaturesReg = ENABLE_SMART;
+	stCIP.irDriveRegs.bSectorCountReg = 1;
+	stCIP.irDriveRegs.bSectorNumberReg = 1;
+	stCIP.irDriveRegs.bCylLowReg = SMART_CYL_LOW;
+	stCIP.irDriveRegs.bCylHighReg = SMART_CYL_HI;
+	stCIP.irDriveRegs.bDriveHeadReg = DRIVE_HEAD_REG | ((ucDriveIndex & 1) << 4);
+	stCIP.irDriveRegs.bCommandReg = SMART_CMD;
+
+	return DeviceIoControl(hDevice, SMART_SEND_DRIVE_COMMAND, &stCIP, sizeof(stCIP) - 1, &stCOP, sizeof(stCOP) - 1, &dwRet, NULL);
+}
+
+bool readSmartAttributes(HANDLE hDevice, DWORD ucDriveIndex, std::shared_ptr<Buffer<PST_ATAOUTPARAM_ATTRIBUTES>> outputBuffer)
+{
+	SENDCMDINPARAMS stCIP = { 0 };
+	SENDCMDOUTPARAMS stCOP = { 0 };
+	DWORD dwRet = 0;
+	BOOL bRet = FALSE;
+
+	stCIP.cBufferSize = READ_ATTRIBUTE_BUFFER_SIZE;
+	stCIP.bDriveNumber = ucDriveIndex & 0xff;
+	stCIP.irDriveRegs.bFeaturesReg = READ_ATTRIBUTES;
+	stCIP.irDriveRegs.bSectorCountReg = 1;
+	stCIP.irDriveRegs.bSectorNumberReg = 1;
+	stCIP.irDriveRegs.bCylLowReg = SMART_CYL_LOW;
+	stCIP.irDriveRegs.bCylHighReg = SMART_CYL_HI;
+	stCIP.irDriveRegs.bDriveHeadReg = DRIVE_HEAD_REG | (((ucDriveIndex & 0xff) & 1) << 4);
+	stCIP.irDriveRegs.bCommandReg = SMART_CMD;
+
+	return DeviceIoControl(hDevice, SMART_RCV_DRIVE_DATA, &stCIP, sizeof(stCIP) - 1, outputBuffer->data(), outputBuffer->size() - 1, &dwRet, NULL);
+}
+
+
+
+bool readSmartThresholds(HANDLE hDevice, DWORD ucDriveIndex, std::shared_ptr<Buffer<PST_ATAOUTPARAM_THRESHOLDS>> outputBuffer)
+{
+	SENDCMDINPARAMS stCIP = { 0 };
+	SENDCMDOUTPARAMS stCOP = { 0 };
+	DWORD dwRet = 0;
+	BOOL bRet = FALSE;
+
+	stCIP.cBufferSize = READ_THRESHOLD_BUFFER_SIZE;
+	stCIP.bDriveNumber = ucDriveIndex & 0xff;
+	stCIP.irDriveRegs.bFeaturesReg = READ_THRESHOLDS;
+	stCIP.irDriveRegs.bSectorCountReg = 1;
+	stCIP.irDriveRegs.bSectorNumberReg = 1;
+	stCIP.irDriveRegs.bCylLowReg = SMART_CYL_LOW;
+	stCIP.irDriveRegs.bCylHighReg = SMART_CYL_HI;
+	stCIP.irDriveRegs.bDriveHeadReg = DRIVE_HEAD_REG | (((ucDriveIndex & 0xff) & 1) << 4);
+	stCIP.irDriveRegs.bCommandReg = SMART_CMD;
+
+	return DeviceIoControl(hDevice, SMART_RCV_DRIVE_DATA, &stCIP, sizeof(stCIP) - 1, outputBuffer->data(), outputBuffer->size() - 1, &dwRet, NULL);
+}
+
+void print_smart_data(std::shared_ptr<Disk> disk)
+{
+	utils::ui::title("S.M.A.R.T data from " + disk->name());
+
+	HANDLE hDisk = CreateFileA(disk->name().c_str(), GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_SYSTEM, 0);
+	if (hDisk != INVALID_HANDLE_VALUE)
+	{
+		GETVERSIONINPARAMS versionInfo = { 0 };
+		DWORD dwRet = 0;
+		bool ret = DeviceIoControl(hDisk, SMART_GET_VERSION, NULL, 0, &versionInfo, sizeof(GETVERSIONINPARAMS), &dwRet, NULL);
+		if (ret)
+		{
+			std::cout << "    Version       : " << std::to_string(versionInfo.bVersion) << " revision " << std::to_string(versionInfo.bRevision) << std::endl;
+			std::cout << "    Type          : " << constants::disk::smart::devicemap_type(versionInfo.bIDEDeviceMap) << std::endl;
+			std::cout << "    Capabilities  : " << constants::disk::smart::capabilities(versionInfo.fCapabilities) << std::endl;
+			std::cout << std::endl;
+
+			if ((versionInfo.fCapabilities & CAP_SMART_CMD) == CAP_SMART_CMD)
+			{
+				if (isSmartEnabled(hDisk, disk->index()))
+				{
+					dwRet = 0;
+					auto attributeBuffer = std::make_shared<Buffer<PST_ATAOUTPARAM_ATTRIBUTES>>(sizeof(ST_ATAOUTPARAM_ATTRIBUTES) + READ_ATTRIBUTE_BUFFER_SIZE);
+					auto thresholdBuffer = std::make_shared<Buffer<PST_ATAOUTPARAM_THRESHOLDS>>(sizeof(ST_ATAOUTPARAM_THRESHOLDS) + READ_THRESHOLD_BUFFER_SIZE);
+
+					if (readSmartAttributes(hDisk, disk->index(), attributeBuffer) && readSmartThresholds(hDisk, disk->index(), thresholdBuffer))
+					{
+						std::shared_ptr<utils::ui::Table> table = std::make_shared<utils::ui::Table>();
+						table->set_margin_left(4);
+
+						table->add_header_line("Index", utils::ui::TableAlign::RIGHT);
+						table->add_header_line("Name");
+						table->add_header_line("Raw Value", utils::ui::TableAlign::RIGHT);
+						table->add_header_line("Normalized Value", utils::ui::TableAlign::RIGHT);
+						table->add_header_line("Worst Value", utils::ui::TableAlign::RIGHT);
+						table->add_header_line("Threshold", utils::ui::TableAlign::RIGHT);
+						table->add_header_line("Status", utils::ui::TableAlign::RIGHT);
+
+						unsigned int nb_attributes = attributeBuffer->data()->cBufferSize / 0xc;
+						PST_SMART_ATTRIBUTE currAttribute = &attributeBuffer->data()->bBuffer[0];
+						PST_SMART_THRESHOLD currThreshold = &thresholdBuffer->data()->bBuffer[0];
+						for (unsigned int i = 0; i < nb_attributes; i++)
+						{
+							if (currAttribute->attributeIndex)
+							{
+								table->add_item_line(std::to_string(currAttribute->attributeIndex));
+								table->add_item_line(constants::disk::smart::attribute_name(currAttribute->attributeIndex));
+								table->add_item_line(std::to_string(currAttribute->rawValue));
+								table->add_item_line(std::to_string(currAttribute->value));
+								table->add_item_line(std::to_string(currAttribute->worst));
+								table->add_item_line(std::to_string(currThreshold->threshold));
+								table->add_item_line(currAttribute->value < currThreshold->threshold ? "Failure" : "Ok");
+								table->new_line();
+							}
+							currAttribute = POINTER_ADD(PST_SMART_ATTRIBUTE, currAttribute, 0xc);
+							currThreshold = POINTER_ADD(PST_SMART_THRESHOLD, currThreshold, 0xc);
+						}
+
+						table->render(std::cout);
+						std::cout << std::endl;
+					}
+					else
+					{
+						std::cerr << "[!] Failed to read S.M.A.R.T attributes." << std::endl;
+					}
+				}
+				else
+				{
+					std::cerr << "[!] S.M.A.R.T is not enabled." << std::endl;
+				}
+			}
+			else
+			{
+				std::cerr << "[!] S.M.A.R.T commands are not supported." << std::endl;
+			}
+		}
+		CloseHandle(hDisk);
+	}
+
+	std::cout << std::endl;
+}
+
+namespace commands {
+
+	namespace smart {
+
+		int print_smart(std::shared_ptr<Options> opts) {
+			std::ios_base::fmtflags flag_backup(std::cout.flags());
+
+			std::shared_ptr<Disk> disk = get_disk(opts);
+
+			if (disk != nullptr)
+			{
+				print_smart_data(disk);
+			}
+
+			std::cout.flags(flag_backup);
+			return 0;
+		}
+	}
+}
--- Sources/Drive/disk.h
+++ Sources/Drive/disk.h
@@ -11,7 +11,118 @@
 #include "volume.h"
 #include "Drive/mbr_gpt.h"
 
-#define DISK_INDEX_IMAGE (-1)
+#define DISK_INDEX_IMAGE			(-1)
+
+#define READ_ATTRIBUTES				0xD0
+#define IOCTL_DISK_BASE				FILE_DEVICE_DISK
+#define SMART_GET_VERSION			CTL_CODE(IOCTL_DISK_BASE, 0x0020, METHOD_BUFFERED, FILE_READ_ACCESS)
+#define SMART_SEND_DRIVE_COMMAND	CTL_CODE(IOCTL_DISK_BASE, 0x0021, METHOD_BUFFERED, FILE_READ_ACCESS | FILE_WRITE_ACCESS)
+#define SMART_RCV_DRIVE_DATA		CTL_CODE(IOCTL_DISK_BASE, 0x0022, METHOD_BUFFERED, FILE_READ_ACCESS | FILE_WRITE_ACCESS)
+#define ID_CMD						0xEC	/* Returns ID sector for ATA*/
+#define IDENTIFY_BUFFER_SIZE		512
+#define SMART_CYL_LOW				0x4F
+#define SMART_CYL_HI				0xC2
+#define DRIVE_HEAD_REG				0xA0
+#define SMART_CMD					0xB0
+
+#define INDEX_ATTRIB_RAW			5
+
+#pragma pack(push, 1)
+
+typedef struct
+{
+	BYTE  bDriverError;
+	BYTE  bIDEStatus;
+	BYTE  bReserved[2];
+	DWORD dwReserved[2];
+} ST_DRIVERSTAT;
+
+typedef struct
+{
+	BYTE attributeIndex;
+	WORD reserved;
+	BYTE value;
+	BYTE worst;
+	DWORD64 rawValue : 48;
+} ST_SMART_ATTRIBUTE, * PST_SMART_ATTRIBUTE;
+
+typedef struct
+{
+	BYTE attributeIndex;
+	BYTE threshold;
+	BYTE reserved[10];
+} ST_SMART_THRESHOLD, * PST_SMART_THRESHOLD;
+
+typedef struct
+{
+	DWORD      cBufferSize;
+	ST_DRIVERSTAT DriverStatus;
+	BYTE       reserved[2];
+	ST_SMART_ATTRIBUTE bBuffer[1];
+} ST_ATAOUTPARAM_ATTRIBUTES, * PST_ATAOUTPARAM_ATTRIBUTES;
+
+typedef struct
+{
+	DWORD      cBufferSize;
+	ST_DRIVERSTAT DriverStatus;
+	BYTE       reserved[2];
+	ST_SMART_THRESHOLD bBuffer[1];
+} ST_ATAOUTPARAM_THRESHOLDS, * PST_ATAOUTPARAM_THRESHOLDS;
+
+typedef struct
+{
+	WORD wGenConfig;
+	WORD wNumCyls;
+	WORD wReserved;
+	WORD wNumHeads;
+	WORD wBytesPerTrack;
+	WORD wBytesPerSector;
+	WORD wSectorsPerTrack;
+	WORD wVendorUnique[3];
+	BYTE sSerialNumber[20];
+	WORD wBufferType;
+	WORD wBufferSize;
+	WORD wECCSize;
+	BYTE sFirmwareRev[8];
+	BYTE sModelNumber[39];
+	WORD wMoreVendorUnique;
+	WORD wDoubleWordIO;
+	WORD wCapabilities;
+	WORD wReserved1;
+	WORD wPIOTiming;
+	WORD wDMATiming;
+	WORD wBS;
+	WORD wNumCurrentCyls;
+	WORD wNumCurrentHeads;
+	WORD wNumCurrentSectorsPerTrack;
+	WORD ulCurrentSectorCapacity;
+	WORD wMultSectorStuff;
+	DWORD ulTotalAddressableSectors;
+	WORD wSingleWordDMA;
+	WORD wMultiWordDMA;
+	BYTE bReserved[127];
+}ST_IDSECTOR;
+
+typedef struct
+{
+	BYTE m_ucAttribIndex;
+	DWORD m_dwAttribValue;
+	BYTE m_ucValue;
+	BYTE m_ucWorst;
+	DWORD m_dwThreshold;
+}ST_SMART_INFO;
+
+typedef struct
+{
+	GETVERSIONINPARAMS m_stGVIP;
+	ST_IDSECTOR m_stInfo;
+	ST_SMART_INFO m_stSmartInfo[256];
+	BYTE m_ucSmartValues;
+	BYTE m_ucDriveIndex;
+	CHAR m_csErrorString[MAX_PATH];
+}ST_DRIVE_INFO;
+
+#pragma pack(pop)
 
 class Disk
 {
