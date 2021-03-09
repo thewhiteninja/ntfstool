d15e0b3f2d3d2b0f9703e0ac4f1278ed161c7686
thewhiteninja <40250307+thewhiteninja@users.noreply.github.com>
2021-03-09 22:15:07 +0100

    complete smart data

--- Sources/Drive/smart.h
+++ Sources/Drive/smart.h
@@ -3,100 +3,6 @@
 #include <WinSock2.h>
 #include <Windows.h>
 
-enum class SmartAttributeID
-{
-	Invalid = 0,
-	ReadErrorRate = 1,
-	ThroughputPerformance = 2,
-	SpinUpTime = 3,
-	StartStopCount = 4,
-	ReallocatedSectorsCount = 5,
-	ReadChannelMargin = 6,
-	SeekErrorRate = 7,
-	SeekTimePerformance = 8,
-	PowerOnHoursCount = 9,
-	SpinRetryCount = 10,
-	CalibrationRetryCount = 11,
-	PowerCycleCount = 12,
-	SoftReadErrorRate = 13,
-	CurrentHeliumLevel = 22,
-	UncorrectableSectorCountReadOrWrite = 160,
-	RemainingSpareBlockPercentage = 161,
-	TotalEraseCount = 164,
-	MaximumEraseCount = 165,
-	MinimumEraseCount = 166,
-	AverageEraseCount = 167,
-	MaxNANDEraseCountFromSpecification = 168,
-	RemainingLifePercentage = 169,
-	AvailableReservedSpace = 170,
-	SSDProgramFailCount = 171,
-	SSDEraseFailCount = 172,
-	SSDWearLevelingCount = 173,
-	UnexpectedPowerLossCount = 174,
-	PowerLossProtectionFailure = 175,
-	EraseFailCount = 176,
-	WearRangeDelta = 177,
-	UsedReservedBlockCountChip = 178,
-	UsedReservedBlockCountTotal = 179,
-	UnusedReservedBlockCountTotal = 180,
-	ProgramFailCountTotalorNon4KAlignedAccessCount = 181,
-	EraseFailCountSamsung = 182,
-	SATADownshiftErrorCount = 183,
-	EndtoEnderror = 184,
-	HeadStability = 185,
-	InducedOpVibrationDetection = 186,
-	ReportedUncorrectableErrors = 187,
-	CommandTimeout = 188,
-	HighFlyWrites = 189,
-	TemperatureDifferencefrom100 = 190,
-	Gsenseerrorrate = 191,
-	PoweroffRetractCount = 192,
-	LoadCycleCount = 193,
-	Temperature = 194,
-	HardwareECCRecovered = 195,
-	ReallocationEventCount = 196,
-	CurrentPendingSectorCount = 197,
-	UncorrectableSectorCount = 198,
-	UltraDMACRCErrorCount = 199,
-	MultiZoneErrorRate = 200,
-	OffTrackSoftReadErrorRate = 201,
-	DataAddressMarkerrors = 202,
-	RunOutCancel = 203,
-	SoftECCCorrection = 204,
-	ThermalAsperityRateTAR = 205,
-	FlyingHeight = 206,
-	SpinHighCurrent = 207,
-	SpinBuzz = 208,
-	OfflineSeekPerformance = 209,
-	VibrationDuringWrite = 211,
-	ShockDuringWrite = 212,
-	DiskShift = 220,
-	GSenseErrorRate = 221,
-	LoadedHours = 222,
-	LoadUnloadRetryCount = 223,
-	LoadFriction = 224,
-	LoadUnloadCycleCount = 225,
-	LoadInTime = 226,
-	TorqueAmplificationCount = 227,
-	PowerOffRetractCycle = 228,
-	LifeCurveStatus = 230,
-	SSDLifeLeft = 231,
-	EnduranceRemaining = 232,
-	MediaWearoutIndicator = 233,
-	AverageEraseCountANDMaximumEraseCount = 234,
-	GoodBlockCountANDSystemFreeBlockCount = 235,
-	HeadFlyingHours = 240,
-	LifetimeWritesFromHostGiB = 241,
-	LiftetimeReadsFromHostGiB = 242,
-	TotalLBAsWrittenExpanded = 243,
-	TotalLBAsReadExpanded = 244,
-	NANDWrites1GiB = 249,
-	ReadErrorRetryRate = 250,
-	MinimumSparesRemaining = 251,
-	NewlyAddedBadFlashBlock = 252,
-	FreeFallProtection = 254
-};
-
 #define DRIVE_HEAD_REG				0xA0
 
 #define SMART_CYL_LOW_BAD			0xF4
@@ -142,7 +48,7 @@ typedef struct _IDINFO
 	struct {
 		USHORT	CHSNumber : 1;
 		USHORT	CycleNumber : 1;
-		USHORT	UnltraDMA : 1;
+		USHORT	UltraDMA : 1;
 		USHORT	Reserved : 13;
 	} wFieldValidity;
 	USHORT	wNumCurCyls;
@@ -156,7 +62,16 @@ typedef struct _IDINFO
 	} wMultSectorStuff;
 	ULONG	dwTotalSectors;
 	USHORT	wSingleWordDMA;
-	USHORT	wMultiWordDMA;
+	struct {
+		USHORT Mode0 : 1;
+		USHORT Mode1 : 1;
+		USHORT Mode2 : 1;
+		USHORT  Reserved1 : 5;
+		USHORT Mode0Sel : 1;
+		USHORT Mode1Sel : 1;
+		USHORT Mode2Sel : 1;
+		USHORT  Reserved2 : 5;
+	} wMultiWordDMA;
 	struct {
 		USHORT	AdvPOIModes : 8;
 		USHORT	Reserved : 8;
