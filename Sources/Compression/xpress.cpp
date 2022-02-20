a4f8e020fe0968bad0137a658807d3fcf10d16e9
thewhiteninja <40250307+thewhiteninja@users.noreply.github.com>
2022-02-20 16:40:22 +0100

    clean xpress code

--- Sources/Compression/xpress.cpp
+++ Sources/Compression/xpress.cpp
@@ -2,59 +2,82 @@
 #include <Compression/ntdll_defs.h>
 #include <Utils/utils.h>
 
+bool parse_chunk_table(std::shared_ptr<Buffer<PBYTE>> compressed, DWORD windows_size, PBYTE* data_start, std::vector<DWORD>& chunks_sizes)
+{
+	chunks_sizes.clear();
+	DWORD last_chunk_size = 0;
+	DWORD current_chunk_size = 1;
+	DWORD chunk_index = 0;
+	int64_t remaining_bytes = compressed->size();
+
+	while (remaining_bytes > 0)
+	{
+		current_chunk_size = compressed->read_at<DWORD>(sizeof(DWORD) * chunk_index++);
+		if (!current_chunk_size)
+		{
+			break;
+		}
+		else
+		{
+			chunks_sizes.push_back(current_chunk_size - last_chunk_size);
+			last_chunk_size = current_chunk_size;
+			remaining_bytes -= sizeof(DWORD) + (current_chunk_size - last_chunk_size);
+		}
+	}
+
+	if (data_start)
+	{
+		*data_start = POINTER_ADD(PBYTE, compressed->data(), sizeof(DWORD) * chunks_sizes.size());
+	}
+
+	return true;
+}
+
+
 int decompress_xpress(std::shared_ptr<Buffer<PBYTE>> compressed, std::shared_ptr<Buffer<PBYTE>> decompressed, DWORD windows_size, DWORD final_size)
 {
-	_RtlDecompressBuffer RtlDecompressBuffer = nullptr;
 	_RtlDecompressBufferEx RtlDecompressBufferEx = nullptr;
 	_RtlGetCompressionWorkSpaceSize RtlGetCompressionWorkSpaceSize = nullptr;
 
-	utils::dll::ntdll::load_compression_functions(&RtlDecompressBuffer, &RtlDecompressBufferEx, &RtlGetCompressionWorkSpaceSize);
+	utils::dll::ntdll::load_compression_functions(nullptr, &RtlDecompressBufferEx, &RtlGetCompressionWorkSpaceSize);
 
-	if (!RtlDecompressBuffer || !RtlDecompressBufferEx || !RtlGetCompressionWorkSpaceSize)
+	if (!RtlDecompressBufferEx || !RtlGetCompressionWorkSpaceSize)
 	{
 		return 1;
 	}
 
-	std::vector<DWORD> chunks_sizes;
-	for (DWORD chunk_index = 0; chunk_index < final_size / windows_size; chunk_index++)
-	{
-		DWORD current_chunk_size = compressed->read_at<DWORD>(sizeof(DWORD) * chunk_index);
-		chunks_sizes.push_back(current_chunk_size);
-	}
-
 	ULONG comp_block = 0, comp_frag = 0;
 	RtlGetCompressionWorkSpaceSize(COMPRESSION_FORMAT_XPRESS_HUFF, &comp_block, &comp_frag);
-	LPVOID workspace = LocalAlloc(LMEM_FIXED, comp_block);
+	std::shared_ptr<Buffer<LPVOID>> workspace = std::make_shared<Buffer<LPVOID>>(comp_block);
 
-	PBYTE compressed_data = POINTER_ADD(PBYTE, compressed->data(), sizeof(DWORD) * chunks_sizes.size());
+	std::vector<DWORD> chunks_sizes;
 	PBYTE decompressed_data = decompressed->data();
+	PBYTE compressed_data = nullptr;
 
-	DWORD prev_chunk_size = 0;
-	for (auto& chunk_size : chunks_sizes)
+	if (parse_chunk_table(compressed, windows_size, &compressed_data, chunks_sizes))
 	{
-		NTSTATUS status;
-		ULONG chunk_final_size = 0;
-		if (status = RtlDecompressBufferEx(COMPRESSION_FORMAT_XPRESS_HUFF, decompressed_data, windows_size, compressed_data, chunk_size - prev_chunk_size, &chunk_final_size, workspace))
+		for (auto& chunk_size : chunks_sizes)
 		{
-			LocalFree(workspace);
-			return 2;
+			NTSTATUS status;
+			ULONG chunk_final_size = 0;
+			if (status = RtlDecompressBufferEx(COMPRESSION_FORMAT_XPRESS_HUFF, decompressed_data, windows_size, compressed_data, chunk_size, &chunk_final_size, workspace->data()))
+			{
+				return 2;
+			}
+			else
+			{
+				decompressed_data = POINTER_ADD(PBYTE, decompressed_data, chunk_final_size);
+				compressed_data = POINTER_ADD(PBYTE, compressed_data, chunk_size);
+			}
 		}
-		else
+
+		DWORD remaining_chunk_size = compressed->size() - static_cast<DWORD>(compressed_data - compressed->address());
+		if (remaining_chunk_size)
 		{
-			decompressed_data = POINTER_ADD(PBYTE, decompressed_data, chunk_final_size);
-			compressed_data = POINTER_ADD(PBYTE, compressed_data, chunk_size - prev_chunk_size);
-			prev_chunk_size = chunk_size;
+			ULONG chunk_final_size = 0;
+			RtlDecompressBufferEx(COMPRESSION_FORMAT_XPRESS_HUFF, decompressed_data, windows_size, compressed_data, remaining_chunk_size, &chunk_final_size, workspace->data());
 		}
 	}
-
-	DWORD remaining_chunk_size = compressed->address() + compressed->size() - compressed_data;
-	if (remaining_chunk_size)
-	{
-		ULONG chunk_final_size = 0;
-		RtlDecompressBufferEx(COMPRESSION_FORMAT_XPRESS_HUFF, decompressed_data, windows_size, compressed_data, remaining_chunk_size, &chunk_final_size, workspace);
-	}
-
-	LocalFree(workspace);
 
 	if (decompressed->size() > final_size)
 	{
