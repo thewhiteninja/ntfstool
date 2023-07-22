8aa1a26d42bd9b5d1d3ac38d882cfd8018578278
thewhiteninja <40250307+thewhiteninja@users.noreply.github.com>
2023-07-23 00:53:09 +0200

    support for large clusters > 64k

--- Sources/NTFS/ntfs_reader.cpp
+++ Sources/NTFS/ntfs_reader.cpp
@@ -3,7 +3,18 @@
 NTFSReader::NTFSReader(std::wstring volume_name, DWORD64 volume_offset) : Reader(volume_name, volume_offset)
 {
 	PBOOT_SECTOR_NTFS pbs = (PBOOT_SECTOR_NTFS)_boot_record;
-	sizes.cluster_size = pbs->bytePerSector * pbs->sectorPerCluster;
+
+	ULONG32 real_sector_per_cluster;
+	if (pbs->sectorPerCluster > 0x80)
+	{
+		real_sector_per_cluster = 1 << -pbs->sectorPerCluster;
+	}
+	else
+	{
+		real_sector_per_cluster = pbs->sectorPerCluster;
+	}
+
+	sizes.cluster_size = pbs->bytePerSector * real_sector_per_cluster;
 	sizes.record_size = pbs->clusterPerRecord >= 0 ? pbs->clusterPerRecord * sizes.cluster_size : 1 << -pbs->clusterPerRecord;
 	sizes.block_size = pbs->clusterPerBlock >= 0 ? pbs->clusterPerBlock * sizes.cluster_size : 1 << -pbs->clusterPerBlock;
 	sizes.sector_size = pbs->bytePerSector;
